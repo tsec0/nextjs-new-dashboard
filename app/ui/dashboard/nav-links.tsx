4c0e835e1c40061305300d2a2b1c611a98cda7c0
Tsvetomir Dikov <46728923+tsec0@users.noreply.github.com>
2025-06-06 12:02:31 +0300

    Just to repeat the training

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,8 +1,16 @@
+'use client'
+
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
+
+// to show an active link to indicate to the user what page they are currently on
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+
+import Link from 'next/link';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -17,19 +25,30 @@ const links = [
 ];
 
 export default function NavLinks() {
+
+  const pathname = usePathname();
+
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <a
+          <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
-          >
+            // wgich link is active and which is not
+            className={
+              clsx(
+              "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
+                {
+                  // here -> pathname === link.href
+                  'bg-sky-100 text-blue-600': pathname === link.href,
+                },
+              )
+            }>
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
         );
       })}
     </>
