9018f8cb7766401620b48b12d2472c60ad5ddab9
Tsvetomir Dikov <46728923+tsec0@users.noreply.github.com>
2025-06-09 13:44:54 +0300

    Removing obsolete code and adding Link

--- auth.ts
+++ auth.ts
@@ -13,7 +13,7 @@ async function getUser(email: string): Promise<User | undefined> {
     const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
     return user[0];
   } catch (error) {
-    console.error('Failed to fetch user:', error);
+    // console.error('Failed to fetch user:', error);
     throw new Error('Failed to fetch user.');
   }
 }
@@ -39,7 +39,7 @@ export const { auth, signIn, signOut } = NextAuth({
           if (passwordsMatch) return user;
         }
  
-        console.log('Invalid credentials');
+        // console.log('Invalid credentials');
         return null;
       },
   })],
